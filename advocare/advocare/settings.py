68a20e634ffea96493e9bac2fbe8b64112424a11
Kartik Prajapati <kartikprajapati9995@gmail.com>
2026-08-30 21:08:34 +0530

    updated changes

--- advocare/advocare/settings.py
+++ advocare/advocare/settings.py
@@ -126,7 +126,6 @@ USE_TZ = True
 # ========== STATIC & MEDIA FILES ==========
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend/build/static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
